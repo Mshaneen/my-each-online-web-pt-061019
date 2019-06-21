72000d4d559bc9293f47dee47b3e20b860c76bbf
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-21 16:41:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+list = ["sh", "an", "een"]
+
+def my_each(array)
+  i = 0
+  while i < array.each
+    yield(array[i])
+    i = i + 1
+  end
 end
+
